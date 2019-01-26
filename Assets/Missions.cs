3df195d45840b3262a3a2f35a49ae565e2a9b9f5
kipar <konovod@gmail.com>
2019-01-27 00:24:58 +0300

    missions to another file lets do thoughts first

---
+++ Assets/Missions.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+abstract public class Mission
+{
+    virtual public void OnInit(MonoBehaviour something)
+    {
+
+    }
+    virtual public void OnFinish(MonoBehaviour something)
+    {
+
+    }
+    abstract public bool Complete(MonoBehaviour something);
+
+}
+
+abstract public class TimedMission : Mission
+{
+    public float time;
+    override public void OnInit(MonoBehaviour something)
+    {
+        base.OnInit(something);
+        time = TheTime(something);
+    }
+
+    abstract public float TheTime(MonoBehaviour something);
+    override public bool Complete(MonoBehaviour something)
+    {
+        time -= Time.deltaTime;
+        return time <= 0;
+    }
+}
+
+public class DemoMission1 : TimedMission
+{
+    override public float TheTime(MonoBehaviour something)
+    {
+        return 10;
+    }
+
+    override public void OnInit(MonoBehaviour something)
+    {
+        base.OnInit(something);
+        Debug.Log("start11");
+    }
+    override public void OnFinish(MonoBehaviour something)
+    {
+        base.OnFinish(something);
+        Debug.Log("end1");
+    }
+}
+
+public class DemoMission2 : TimedMission
+{
+    override public float TheTime(MonoBehaviour something)
+    {
+        return 3;
+    }
+
+    override public void OnInit(MonoBehaviour something)
+    {
+        base.OnInit(something);
+        Debug.Log("start2");
+    }
+    override public void OnFinish(MonoBehaviour something)
+    {
+        base.OnFinish(something);
+        Debug.Log("end2");
+    }
+}
+
+
--- Assets/Scenario.cs
+++ Assets/Scenario.cs
@@ -1,77 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using Missions;
 
 //класс отвечает за исполнение и переключение миссий.
-
-abstract public class Mission
-{
-    virtual public void OnInit(MonoBehaviour something)
-    {
-
-    }
-    virtual public void OnFinish(MonoBehaviour something)
-    {
-
-    }
-    abstract public bool Complete(MonoBehaviour something);
-
-}
-
-abstract public class TimedMission : Mission
-{
-    public float time;
-    override public void OnInit(MonoBehaviour something)
-    {
-        base.OnInit(something);
-        time = TheTime(something);
-    }
-
-    abstract public float TheTime(MonoBehaviour something);
-    override public bool Complete(MonoBehaviour something)
-    {
-        time -= Time.deltaTime;
-        return time <= 0;
-    }
-}
-
-public class DemoMission1 : TimedMission
-{
-    override public float TheTime(MonoBehaviour something)
-    {
-        return 10;
-    }
-
-    override public void OnInit(MonoBehaviour something)
-    {
-        base.OnInit(something);
-        Debug.Log("start1");
-    }
-    override public void OnFinish(MonoBehaviour something)
-    {
-        base.OnFinish(something);
-        Debug.Log("end1");
-    }
-}
-
-public class DemoMission2 : TimedMission
-{
-    override public float TheTime(MonoBehaviour something)
-    {
-        return 3;
-    }
-
-    override public void OnInit(MonoBehaviour something)
-    {
-        base.OnInit(something);
-        Debug.Log("start2");
-    }
-    override public void OnFinish(MonoBehaviour something)
-    {
-        base.OnFinish(something);
-        Debug.Log("end2");
-    }
-}
 
 
 public class Scenario : MonoBehaviour
