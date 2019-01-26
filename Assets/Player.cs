1c892b01286fb76f727b4eb542aeb9f9514ca7d4
kipar <konovod@gmail.com>
2019-01-26 12:19:33 +0300

    Подбирание и выкидывание объектов

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 4;
+    [SerializeField] private bool hands = false;
+    [SerializeField] private Transform handsPosition;
+    [SerializeField] private Transform dropPosition;
+    [SerializeField] private GameObject currentItem;
+    [SerializeField] private List<Transform> nearest = new List<Transform>();
+
     
     void Update()
     {
         Move();
+        TakeAndPutItem();
     }
 
     void Move()
@@ -17,11 +24,57 @@ public class Player : MonoBehaviour
         transform.Translate(Vector3.right * horizontal);
     }
 
+    void TakeAndPutItem()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!hands)
+            {
+                //Take
+                if (nearest.Count > 0)
+                {
+                    int rand = Random.Range(0, nearest.Count);
+
+                    currentItem = nearest[rand].gameObject;
+                    currentItem.transform.SetParent(handsPosition);
+                    currentItem.transform.position = handsPosition.position;
+
+                    nearest.Remove(nearest[rand]);
+
+                    hands = true;
+                }
+            }
+            else
+            {
+                //Drop
+                GameObject drop = Instantiate(currentItem, dropPosition.position, transform.rotation);
+                drop.name = currentItem.name;
+                drop.transform.localScale = 
+                    new Vector3(drop.transform.localScale.x, drop.transform.localScale.y * 2, 
+                    drop.transform.localScale.z);
+                Destroy(currentItem);
+                currentItem = null;
+
+                hands = false;
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Item"))
         {
-            Debug.Log("Премет: " + name);
+            nearest.Add(col.transform.parent);
+            Debug.Log("Премет: " + col.name);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Item"))
+        {
+            nearest.Remove(col.transform.parent);
+            Debug.Log("Премет: " + col.name);
         }
     }
 }
