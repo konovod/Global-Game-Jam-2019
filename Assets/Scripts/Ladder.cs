c44c042877e5bb5df82b0a9d2413713b772a91a2
kipar <konovod@gmail.com>
2019-01-27 01:15:10 +0300

    sort of fixed ladder

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -15,6 +15,8 @@ public class Ladder : MonoBehaviour
     [SerializeField] private Transform ladderTop;
     [SerializeField] private Transform ladderBottom;
 
+    public bool wasMovingDown = false;
+
     private void Awake()
     {
         instance = this;
@@ -27,16 +29,27 @@ public class Ladder : MonoBehaviour
         inProgress = !nearBottom && !nearTop;
         float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         v.y += vertical;
+        //if (vertical < 0)
+        //    wasMovingDown = true;
+        //if (vertical > 0)
+        //    wasMovingDown = false;
         if (vertical != 0)
             Player.instance.UsedWS = true;
 
 
         nearTop = v.y >= ladderTop.position.y - 0.00001;
+        //if (!wasMovingDown && v.y >= ladderTop.position.y - 0.15)
+        //    nearTop = true;
         nearBottom = v.y <= ladderBottom.position.y + 0.00001;
+        //if (wasMovingDown && v.y <= ladderBottom.position.y + 0.15)
+        //    nearBottom = true;
 
         if (nearTop)
             v.y = ladderTop.position.y;
-        
+
+        if (nearBottom)
+            v.y = ladderBottom.position.y;
+
         Player.instance.transform.position = v;
     }
 
