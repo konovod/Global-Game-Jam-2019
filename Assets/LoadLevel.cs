33f3fa3773c03fa7422687d8d1fa761007333ce3
SanjarKariev <kariev.s69@mail.ru>
2019-01-26 14:10:12 +0300

    loadlevel

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadLevel : MonoBehaviour {
+
+    public Button LoadButton;
+
+    public UnityAction TaskOnClick { get; private set; }
+
+    void Start () {
+		//Button btn = LoadButton.GetComponent<Button>();
+        LoadButton.onClick.AddListener(() => QuitOnClick());
+    }
+
+    public void QuitOnClick()
+    {
+        Application.Quit(); //здесь пытаюсь закрыть сцену   
+        LoadGame();
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Scenes/meny");
+    }
+}
